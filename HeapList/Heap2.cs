937f91e238a462f61952ddb07ed990b283aa3e2c
psgivens <psgivens11@yahoo.com>
2014-07-07 07:05:54 -0700

    Some work with Heaps.

---
+++ HeapList/Heap2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeapList
+{
+    public class Heap2
+    {
+        private List<int> _list = new List<int>();
+
+        public Heap2(int[] array)
+        {
+            foreach (var value in array)
+            {
+                Insert(value);
+            }
+        }
+
+        private void Insert(int value)
+        {
+            _list.Add(value);
+            var count = _list.Count;
+            if (count == 1) return;
+
+            int lastIndex = count - 1;
+            int i = SwapWithParent(lastIndex);
+            while (i != lastIndex && i != 0)
+            {
+                lastIndex = i;
+                i = SwapWithParent(i);
+            }
+        }
+
+        private int SwapWithParent(int index)
+        {
+            if (index == 0) return 0;
+            int parentIndex = GetParentIndex(index);
+            int parent = _list[parentIndex];
+            int current = _list[index];
+            if (current > parent) return index;
+
+            _list[parentIndex] = current;
+            _list[index] = parent;
+            return parentIndex;
+        }
+
+        private int GetParentIndex(int index) { return (index - 1) >> 1; }
+        private int GetLeftIndex(int index) { return (index << 1) + 1; }
+        private int GetRightIndex(int index) { return (index >> 1) + 2; }
+
+        public IEnumerable<int> Values { get { return _list; } }
+
+        internal int Pop()
+        {
+            int value = _list[0];
+            int lastIndex = _list.Count - 1;
+            int last = _list[lastIndex];
+            _list[0] = last;
+            _list.RemoveAt(lastIndex);
+
+            int finalIndex = _list.Count - 1;
+
+            lastIndex = 0;
+            int index = SwapWithChild(0);
+            while(index != lastIndex && lastIndex < finalIndex)
+            {
+                lastIndex = index;
+                index = SwapWithChild(index);
+            }
+
+            return value;
+        }
+
+        private int SwapWithChild(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HeapList/HeapUnitTest1.cs
+++ HeapList/HeapUnitTest1.cs
@@ -21,9 +21,31 @@ namespace HeapList
         }
 
         [TestMethod]
-        public void TestHeapRemoval()
+        public void TestHeap2Construction()
         {
-            Assert.Fail("TestHeapRemoval not implemented");
+            var array = new int[] { 34, 92, 83, 45, 12, 2, 4, 94 };
+            var sorted = new int[] { 2, 34, 4, 92, 45, 83, 12, 94 };
+            var heap = new Heap2(array);
+            var values = heap.Values.ToArray();
+            for (int i = 0; i < values.Length; i++)
+            {
+                Assert.IsTrue(values[i] == sorted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestHeap2Removal()
+        {
+            var array = new int[] { 34, 92, 83, 45, 12, 2, 4, 94 };
+            var sorted = new int[] { 2, 34, 4, 92, 45, 83, 12, 94 };
+            var heap = new Heap2(array);
+            int index = 0;
+            while(heap.Values.Any())
+            {
+                int current = heap.Pop();
+                int expected = sorted[index];
+                Assert.IsTrue(current == expected);
+            }
         }
     }
 }
